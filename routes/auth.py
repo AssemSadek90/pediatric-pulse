d72b2c299b8d1db6dd329458238e5c03fcee1ccb
Omar Saied <74738267+3marsaied@users.noreply.github.com>
2024-04-28 01:23:12 +0300

    update

--- routes/auth.py
+++ routes/auth.py
@@ -44,11 +44,11 @@ async def user_login(login_info: schemas.UserLogin, db: session = Depends(DataBa
                     raise HTTPException(status_code=403, detail="User not found")
                 
     
-    # if not utils.verify_password(login_info.password, user.password):
-    #     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect password")
-
-    if login_info.password != user.password:
+    if not utils.verify_password(login_info.password, user.password):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect password")
+
+    # if login_info.password != user.password:
+    #     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect password")
 
     # Create an access token
     access_token = oauth2.create_access_token(data={"user_id": user.userId, "type": "user"})
