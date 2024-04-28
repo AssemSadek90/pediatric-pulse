c4b4875709ef7c4cec8ec6817685a2d572d1a007
Omar Saied <74738267+3marsaied@users.noreply.github.com>
2024-04-28 17:52:34 +0300

    ..

--- oauth2.py
+++ oauth2.py
@@ -32,7 +32,7 @@ def create_access_token(data: dict):
 
     return encoded_iwt
 
-
+import jwt
 def verify_access_token(token: str, credentials_exception=None):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
