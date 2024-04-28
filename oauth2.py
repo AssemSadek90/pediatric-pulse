e3ad6ef59bc3745ab55ca58a53c3f5efb701d43d
Omar Saied <74738267+3marsaied@users.noreply.github.com>
2024-04-28 15:21:39 +0300

    ...

--- oauth2.py
+++ oauth2.py
@@ -1,19 +1,18 @@
 from fastapi import Depends, HTTPException
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
-from schemas import tokenData
+from . import schemas, database, models
 from fastapi.security import OAuth2PasswordBearer
-import jwt.exceptions
-from pydantic import BaseModel, ValidationError
-
+from sqlalchemy.orm import session
+from .routes import users
 
 
 
 oauth2_schema = OAuth2PasswordBearer(tokenUrl='login')
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b822545618e8d3e992e3esgsfe312edw"
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 525600
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 
 def create_access_token(data: dict):
@@ -24,7 +23,6 @@ def create_access_token(data: dict):
     encoded_iwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
     return encoded_iwt
-
 
 
 def verify_access_token(token: str, credentials_exception=None):
@@ -51,8 +49,8 @@ def verify_access_token(token: str, credentials_exception=None):
         else:
             return {"message": "Invalid token"}
 
-
-        
-def get_current_user(token:str = Depends(oauth2_schema)):
+def get_current_user(token:str = Depends(oauth2_schema), db: session = Depends(database.get_db())):
     credentials_exception = HTTPException(status_code=401, detail= "couldent validate credentials", headers={"www-Authenticate":"Bearer"})
-    return verify_access_token(token, credentials_exception)
+    token = verify_access_token(token, credentials_exception)
+    user = users.getUserById(token.id, db)
+    return user
