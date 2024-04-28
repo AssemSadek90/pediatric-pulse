904258f465d0e5ecabe63c0598063360d747e02d
Omar Saied <74738267+3marsaied@users.noreply.github.com>
2024-04-28 18:04:10 +0300

    ..

--- oauth2.py
+++ oauth2.py
@@ -4,7 +4,6 @@ from datetime import datetime, timedelta
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import session
 
-
 import sys
 sys.path.append('BackEnd')
 
@@ -15,32 +14,37 @@ import utils
 import schemas
 from routes import user
 
-
-oauth2_schema = OAuth2PasswordBearer(tokenUrl='login')
-
+# Define your SECRET_KEY and ALGORITHM
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
-
+# Create an access token
 def create_access_token(data: dict):
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({'exp': expire})
 
-    encoded_iwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_token = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_token
 
-    return encoded_iwt
-
-import jwt
+# Verify an access token
 def verify_access_token(token: str, credentials_exception=None):
-    payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-    user_id: str = payload.get("user_id")
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: str = payload.get("user_id")
 
-    if not user_id:
+        if not user_id:
+            if credentials_exception:
+                raise credentials_exception
+            else:
+                return {"message": "User ID not found in token"}
+
+        return {"id": user_id}
+    except JWTError:
         if credentials_exception:
             raise credentials_exception
         else:
-            return {"message": "User ID not found in token"}
+            return {"message": "Invalid token"}
 
-    return {"id": user_id}  # Return a dictionary with 'id' key
+
