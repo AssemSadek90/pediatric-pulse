38d1a52eb94dbcb18bf72ade0b3a09714b1562ba
Omar Saied <74738267+3marsaied@users.noreply.github.com>
2024-05-03 19:45:02 +0300

    ..

--- routes/MedicalRecord.py
+++ routes/MedicalRecord.py
@@ -18,13 +18,13 @@ router = APIRouter(
 
 
 @router.get('/get/medicalRecord/{patientId}/{parentId}', description='this get request returns all the medical records for a certain patient', response_model=list[schemas.medicalRecordResponse])
-async def getMedicalRecord(patienId: int, parentId: int,token: str, db: session = Depends(DataBase.get_db)):
+async def getMedicalRecord(patientId: int, parentId: int,token: str, db: session = Depends(DataBase.get_db)):
     token_data = oauth2.verify_access_token(parentId, token)
     if not token_data:
         return {"message": "unauthorized"}
     if token_data == False:
         return {"message": "unauthorized"}
-    medicalRecords = db.query(models.MedicalRecord).filter(models.MedicalRecord.patientId == patienId).all()
+    medicalRecords = db.query(models.MedicalRecord).filter(models.MedicalRecord.patientId == patientId).all()
     patients = []
     for patient in medicalRecords:
         patient.createdAt = patient.createdAt.isoformat()  # Convert datetime to string
