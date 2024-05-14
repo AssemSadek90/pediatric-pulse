1ab138fd8dd5f0c92b2294955066c74bd1a56d07
Omar Saied <74738267+3marsaied@users.noreply.github.com>
2024-05-15 00:45:29 +0300

    ...

--- routes/reviews.py
+++ routes/reviews.py
@@ -50,12 +50,12 @@ async def get_review(doctorId: int, db: session = Depends(DataBase.get_db)):
 
 @router.get("/get/reviews/barchart/{doctorId}", status_code=status.HTTP_200_OK, description="This is a get request to get all reviews of a patient", response_model=List[schemas.barChart])
 async def get_reviews_barchart(doctorId: int, token: str, db: session = Depends(DataBase.get_db)):
-    token = oauth2.verify_access_token(doctorId, token)
-    if not token:
+    token_data = oauth2.verify_access_token(doctorId, token)
+    if not token_data:
         raise HTTPException( status_code=401, detail= "unauthorized")
     reviewsData = []
-    i = 1
-    while i < 6:
+    i = 5
+    while i > 0:
         reviews = db.query(models.reviews).filter(models.reviews.doctorId == doctorId, models.reviews.rating == i).all()
         reviewData = {
             "number": len(reviews),
@@ -64,3 +64,21 @@ async def get_reviews_barchart(doctorId: int, token: str, db: session = Depends(
         reviewsData.append(reviewData)
         i += 1
     return reviewsData
+
+
+
+@router.get("/get/doctor/avgRating/{doctorId}", description="This route returns the average rating of a doctor", response_model=schemas.avgRating)
+async def getAvgRating(doctorId: int, token:str, db: session = Depends(DataBase.get_db)):
+    token_data = oauth2.verify_access_token(doctorId, token)
+    if not token_data:
+        raise HTTPException( status_code=401, detail= "unauthorized")
+    totalRating = 0
+    reviwes = db.query(models.reviews).filter(models.reviews.doctorId == doctorId).all()
+    for review in reviwes:
+        totalRating += review.rating
+    avgRating = totalRating/len(reviwes)
+    returnedData = {
+        "avgRating": float(avgRating),
+        "count": len(reviwes),
+    }
+    return returnedData
