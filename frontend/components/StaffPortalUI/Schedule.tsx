47459fe5623dd9562534fba2bb49a2b9cbb7a17e
MostafaKhaled <MostafaDarwish9392@gmail.com>
2024-05-17 15:49:19 +0300

    feat: :sparkles: Added Appointment table

--- frontend/components/StaffPortalUI/Schedule.tsx
+++ frontend/components/StaffPortalUI/Schedule.tsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+function createData(
+  name: string,
+  calories: number,
+  fat: number,
+  carbs: number,
+  protein: number,
+) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
 
 const Schedule = () => {
+  
+
   return (
-    <div className= 'm-5 w-screen rounded-md shadow-lg flex-col   bg-zinc-200'>
-      Table
+    <div className= 'm-5 w-screen  flex-col'>
+       <TableContainer   component={Paper}>
+      <Table sx={{ minWidth: 600 }} aria-label="simple table">
+        <TableHead>
+          <TableRow >
+            <TableCell  > <span className=' text-orange-500 text-lg font-semibold '>Patient Name</span></TableCell>
+            <TableCell align="right"><span className=' text-orange-500 text-lg font-semibold '>Day</span></TableCell>
+            <TableCell align="right"><span className=' text-orange-500 text-lg font-semibold '>From</span></TableCell>
+            <TableCell align="right"><span className=' text-orange-500 text-lg font-semibold '>To</span></TableCell>
+            <TableCell align="right"><span className=' text-orange-500 text-lg font-semibold '>Doctor</span></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
     </div>
   )
 }
