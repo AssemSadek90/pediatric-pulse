bcb1d1cbe8613e4b5e3bf2dfd51330cb04a4a1a1
Omar Saied <74738267+3marsaied@users.noreply.github.com>
2024-05-20 01:17:17 +0300

    ...

--- routes/appointment.py
+++ routes/appointment.py
@@ -107,6 +107,40 @@ async def get_appointment(doctorId: int, userId: int, token: str, db: session = 
     return Data
 
 
+
+@router.get('/get/all/appointments', description="This route returns all appointments")
+async def get_appointment(adminId: int, token: str, db: session = Depends(DataBase.get_db)):
+    
+    appointments = db.query(models.Appointment).all()
+    Data = []
+    
+    for appointment in appointments:
+        user = db.query(models.User).filter(models.User.userId == appointment.parentId).first()
+        patient = db.query(models.Patient).filter(models.Patient.id == appointment.patientId).first()
+        doctor = db.query(models.Doctor).filter(models.Doctor.id == appointment.doctorId).first()
+        
+        appointment_data = {
+            "appointmentId": appointment.id,
+            "parentId": user.userId,
+            "patientId": patient.id,
+            "doctorId": doctor.id,
+            "patientFirstName": patient.firstName,
+            "parentFirstName": user.firstName,
+            "parentLastName": user.lastName,
+            "doctorFirstName": doctor.firstName,
+            "doctorLastName": doctor.lastName,
+            "parentPic": user.profilePicture,
+            "appointmentDate": appointment.appointmentDate,
+            "From": appointment.From,
+            "To": appointment.To,
+            "isTaken": appointment.isTaken,
+        }
+        
+        Data.append(appointment_data)
+    
+    return Data 
+
+
 @router.get("/get/all/appointments/table/{adminId}", status_code=status.HTTP_200_OK, description="This is a get request to get all appointments of a patient")
 async def get_appointment(adminId: int, token: str, db: session = Depends(DataBase.get_db)):
     token_data = oauth2.verify_access_token(adminId, token)
